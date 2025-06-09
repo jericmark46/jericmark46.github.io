9d0ab942e4dfabe0ffd9ae52014340a0a701fffa
jericmark46 <jericmark46@gmail.com>
2025-06-09 22:47:25 +0800

    initialized update (22:47 9.6.2025)

--- script.js
+++ script.js
@@ -12,9 +12,29 @@ window.onscroll = () => {
 }
 
 const typed = new Typed('.multiple-text', {
-      strings: ['Work Student', 'Video Editor','Graphic Designer','Photographer','Web Developer', 'Programmer', 'Cisco Networker'],
-      typeSpeed: 80,
-      backSpeed: 80,
-      backDelay: 1200,
-      loop: true,
+    strings: ['Work Student', 'Video Editor', 'Graphic Designer', 'Photographer', 'Web Developer', 'Programmer', 'Cisco Networker'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1200,
+    loop: true,
+});
+
+
+
+// Fullscreen Gallery Functionality
+const galleryImages = document.querySelectorAll('.gallery img');
+const fullscreenOverlay = document.getElementById('fullscreenOverlay');
+const fullscreenImage = document.getElementById('fullscreenImage');
+
+galleryImages.forEach(img => {
+    img.addEventListener('click', () => {
+        fullscreenImage.src = img.src;
+        fullscreenOverlay.style.display = 'flex';
     });
+});
+
+fullscreenOverlay.addEventListener('click', () => {
+    fullscreenOverlay.style.display = 'none';
+    fullscreenImage.src = '';
+});
+
